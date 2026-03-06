18d3d3afaa22bf4cc819aff8dc717bde8054fcf8
EA-Soft <botee2020@gmail.com>
2026-03-06 15:42:59 +0000

    Your descriptive message here

--- backupage.php
+++ backupage.php
@@ -1,49 +1,47 @@
 <?php
 include('server/config.php');
 
-$tables = atray();
-$rables = array();
-$result =  = $r$sulc->fetonnPDO::FETCH_NUM)){
-  $tables[] = $row[0];
-}->query("SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'");
-while($row = $result->fetch(PDO::FETCH_NUM)){
-  $tables[] = $row[0];
+// Get all table names
+$tables = array();
+$result = $conn->query("SELECT name FROM sqlite_master WHERE type='table' AND name NOT LIKE 'sqlite_%'");
+while ($row = $result->fetch(PDO::FETCH_NUM)) {
+    $tables[] = $row[0];
 }
 
 $return = '';
-  
-  $returnf.=o'DROP TABLE IF EXISTS '.$table.';';
-  $row2 = $conn->query("SELECT sql FROM sqlreach($tables as $table){
-  $result = $conn->query("SELECT * FROM ".$table);
-  $num_fields = $result->columnCount();
-  
-  $return .= 'DROP TABL){
-      $return .= "INSERT INTO ".$table." VALUES(";
-      for($j=0;$j<$num_fields;$j++E 
-        $row[$j] = addslaIF EXISTS '.$table.';';
-  $row2 = $conn->query("SELECT sql FROM sqlite_master WHERE name='".$table."'")->fetch(PDO::FETCH_NUM);
-  $return .= "\n\n".$row2[0]';}
-        if($j<$num_fields-1){ $return .= ',."}
-      ;\n\n";
-      $return .= ");\n";
-  $rows = $result->fetchAll(PDO::FETCH_NUM);
-  foreach($rows as $row){
-      $return .= "INSERT INTO ".$table." VALUES(";
-      for($j=0;$j<$num_fields;$j++){
-        $row[$j] = addslashes($row[$j]);
-        if(isset($row[$j])){ $return .= '"'.$row[$j].'"';}
-        else{ $return .= '""';}
-        if($j<$num_fields-1){ $return .= ',';}
-      }
-      $re
-turn .= ");\n";
-  }
-  $return .= "\n\n\n";
+
+// Loop through each table
+foreach ($tables as $table) {
+    // Get table structure
+    $return .= 'DROP TABLE IF EXISTS ' . $table . ';';
+    $row2 = $conn->query("SELECT sql FROM sqlite_master WHERE name='" . $table . "'")->fetch(PDO::FETCH_NUM);
+    $return .= "\n\n" . $row2[0] . ";\n\n";
+
+    // Get table data
+    $result = $conn->query("SELECT * FROM " . $table);
+    $num_fields = $result->columnCount();
+    $rows = $result->fetchAll(PDO::FETCH_NUM);
+
+    foreach ($rows as $row) {
+        $return .= "INSERT INTO " . $table . " VALUES(";
+        for ($j = 0; $j < $num_fields; $j++) {
+            $row[$j] = addslashes($row[$j]);
+            if (isset($row[$j])) {
+                $return .= '"' . $row[$j] . '"';
+            } else {
+                $return .= '""';
+            }
+            if ($j < ($num_fields - 1)) {
+                $return .= ',';
+            }
+        }
+        $return .= ");\n";
+    }
+    $return .= "\n\n\n";
 }
 
-//save file
-//$f=date('d-m-Y');
-$handle = fopen("D:\buckup.sql","w+");
-fwrite($handle,$return);
+// Save the SQL file
+$handle = fopen("D:/buckup.sql", "w+");
+fwrite($handle, $return);
 fclose($handle);
 echo "1";
